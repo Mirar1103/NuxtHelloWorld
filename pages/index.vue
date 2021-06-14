172b640127f851d6ffa821f437c500f8ae9e3ca9
maximilian <Maximilian.Schug@cib.de>
2021-06-14 15:34:38 +0200

    added index

--- pages/index.vue
+++ pages/index.vue
@@ -71,7 +71,13 @@
 
   }
 
-
+  export default {
+    head() {
+      return {
+        script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+      };
+    },
+  };
 
 </script>
 
